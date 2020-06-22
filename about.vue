61979eb3f3d1eb4794087b8db512de4a2c0b291a
Spider-dev20 <67261837+Spider-dev20@users.noreply.github.com>
2020-06-22 13:49:01 +0200

    my files to the pages

---
+++ about.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+    <nuxt-link to="/" >Home Page </nuxt-link>
+    
+    <p class="menu">about</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"about"
+}
+</script>
+<style scoped>
+    .menu{
+        font-size: 45px;
+        color: red;
+    }
+</style>
---
+++ index.vue
@@ -0,0 +1,29 @@
+<template>
+<body>
+  <div>
+    <nuxt-link to="/about" >Food-Menu </nuxt-link>
+    <h1>LEONARDO DA VINCI</h1>
+    <h1>FOOD IS ART</h1>
+    <img src="./Leonardo.jpg" alt="">
+  </div>
+</body>
+</template>
+
+<script>
+export default {
+  name:"index" 
+}
+</script>
+<style>
+  body {
+    background-color: burlywood;
+    text-align: center;
+  }
+  h1 {
+    font-family: initial;
+    border: aqua 25px;
+    padding-block: aqua 25px;
+    margin-block: aqua 24px;
+  }
+  
+</style>
